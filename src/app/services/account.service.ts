ac915a677df1b4b97170b488adbb0f2d96d67101
Lesedi Mathabatha Mokonyama <93593911+LesediMoko@users.noreply.github.com>
2024-07-20 11:55:09 +0200

    feat: :sparkles: added endpoint to get transactions

--- src/app/services/account.service.ts
+++ src/app/services/account.service.ts
@@ -1,14 +1,21 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment.development';
+import { Observable } from 'rxjs';
+import { IAccountBalanceResponse, ITransactionsResponse } from '../models/api-types';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AccountService {
-  baseUrl = `${environment.host}/za/pb/v1/accounts/`
+  baseUrl = `${environment.host}za/pb/v1/accounts`
+  dateRangeQueryParam = "fromDate=2022-11-01&toDate=2023-02-10"
 
   constructor(private http: HttpClient) {
     
    }
+
+  getTransactions(accountId : string) : Observable<ITransactionsResponse> {
+    return this.http.get(`${this.baseUrl}/${accountId}/transactions?${this.dateRangeQueryParam}`, {headers : {"host" : environment.host} }) as Observable<ITransactionsResponse>;
+  }
 }
