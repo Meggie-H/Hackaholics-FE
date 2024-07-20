59e6e49d367f049454b586b5967e9d2ba73dcfae
Lesedi Mathabatha Mokonyama <93593911+LesediMoko@users.noreply.github.com>
2024-07-20 14:53:55 +0200

    feat: :lipstick: added icons

--- src/app/components/beneficiaries-card/beneficiaries-card.component.ts
+++ src/app/components/beneficiaries-card/beneficiaries-card.component.ts
@@ -2,11 +2,14 @@ import { Component, Input } from '@angular/core';
 import { IBeneficiaryCardInfo } from '../../models/app-types';
 import { IBeneficiary } from '../../models/api-types';
 import { mockBeneficiary } from '../../utilities/mocks';
+import { NgIconComponent, provideIcons } from '@ng-icons/core';
+import {iconoirWarningTriangle} from '@ng-icons/iconoir'
 
 @Component({
   selector: 'app-beneficiaries-card',
   standalone: true,
-  imports: [],
+  imports: [NgIconComponent],
+  viewProviders: [provideIcons({iconoirWarningTriangle })],
   templateUrl: './beneficiaries-card.component.html',
   styleUrl: './beneficiaries-card.component.css'
 })
