46226eb2533edfb26536fb4f20fa68dc57816dbb
Lesedi Mathabatha Mokonyama <93593911+LesediMoko@users.noreply.github.com>
2024-07-20 13:16:14 +0200

    feat: :lipstick: icons added

--- src/app/components/transaction-cards/transaction-cards.component.ts
+++ src/app/components/transaction-cards/transaction-cards.component.ts
@@ -1,10 +1,13 @@
 import { Component, Input } from '@angular/core';
 import { ITransactionCardInfo } from '../../models/app-types';
+import { NgIconComponent, provideIcons } from '@ng-icons/core'
+import {matQueryBuilder} from '@ng-icons/material-icons/baseline'
 
 @Component({
   selector: 'app-transaction-cards',
   standalone: true,
-  imports: [],
+  imports: [NgIconComponent],
+  viewProviders: [provideIcons({ matQueryBuilder })],
   templateUrl: './transaction-cards.component.html',
   styleUrl: './transaction-cards.component.css'
 })
